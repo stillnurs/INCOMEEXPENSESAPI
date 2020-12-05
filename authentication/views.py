ea79b582b6c11bd0a66067bb5001f85a093d3fcc
stillnurs <noorsultan.mamataliev@gmail.com>
2020-12-06 03:05:32 +0600

    first commit, user registration created

---
+++ authentication/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import RegisterSerializer
+from .models import User
+from .utils import Utils
+
+
+class RegisterView(generics.GenericAPIView):
+
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        user = User.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user)
+
+        current_site = get_current_site(request).domain
+        relativelink = reverse('email-verify')
+
+        absurl = 'http://' + current_site + relativelink + '?token=' + str(token)
+        email_body = 'Hi ' + user.username + 'Use link below to verify your email \n' + absurl
+        data = {'email_body': email_body, 'to_email': user.email,
+                'email_subject': 'Verify your email'}
+
+        Utils.send_email(data)
+
+        return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass
