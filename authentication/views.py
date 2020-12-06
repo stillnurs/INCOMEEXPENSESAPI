e672c19d1910bfc5916f54a9a6c828853938d7d0
stillnurs <noorsultan.mamataliev@gmail.com>
2020-12-07 00:50:09 +0600

    added email verification web ui form

--- authentication/views.py
+++ authentication/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 from django.conf import settings
+from drf_yasg import openapi
 
-from rest_framework import generics, status
+from rest_framework import generics, status, views
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 import jwt
+from drf_yasg.utils import swagger_auto_schema
 
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, EmailVerificationSerializer
 from .models import User
 from .utils import Utils
 
@@ -39,7 +40,12 @@ class RegisterView(generics.GenericAPIView):
         return Response(user_data, status=status.HTTP_201_CREATED)
 
 
-class VerifyEmail(generics.GenericAPIView):
+class VerifyEmail(views.APIView):
+    serializer_class = EmailVerificationSerializer
+    token_param_config = openapi.Parameter(
+        'token', in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
+
+    @swagger_auto_schema(manual_parameters=[token_param_config])
     def get(self, request):
         token = request.GET.get('token')
         try:
